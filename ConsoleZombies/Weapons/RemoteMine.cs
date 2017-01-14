ed5c82fb4e8e01a01c1890e5d34737ad96b1994f
Adam Abdelhamed <adamabdelhamed@live.com>
2017-01-14 09:58:54 -0800

    initial commit - won't build until I update PowerArgs and add the NuGet package here

---
+++ ConsoleZombies/Weapons/RemoteMine.cs
@@ -0,0 +1,17 @@
+﻿using PowerArgs.Cli.Physics;
+
+namespace ConsoleZombies
+{
+    public class RemoteMine : Explosive
+    {
+        public RemoteMine(Rectangle bounds, float angleIcrement, float range) : base(bounds, angleIcrement, range)
+        {
+
+        }
+
+        public void Detonate()
+        {
+            Explode();
+        }
+    }
+}
